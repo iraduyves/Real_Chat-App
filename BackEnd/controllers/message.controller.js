7015bc7d3c08b5ed0c47b34a5788e04f5fdcad67
Yves IRADUKUNDA <iradukundayves11@gmail.com>
2024-02-12 20:05:52 +0200

    feat:implementing Backend

--- BackEnd/controllers/message.controller.js
+++ BackEnd/controllers/message.controller.js
@@ -49,7 +49,7 @@ export const getMessage = async (req, res) => {
         {
             return res.status(200).json([]);
         }
-kjkjdbkgbrjkegbj
+kjkjdbkgbrjkegb
         res.status(200).json(conversation.message);
     } 
     catch (error)
