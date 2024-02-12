081788b66945dd76bdcac13ee17e7bff242a6c6a
Yves IRADUKUNDA <iradukundayves11@gmail.com>
2024-02-12 19:50:40 +0200

    feat:implementing Backend

--- BackEnd/controllers/message.controller.js
+++ BackEnd/controllers/message.controller.js
@@ -1,11 +1,55 @@
+import Conversation from "../Models/conversation.model.js";
+import Message from "../Models/mesage.model.js";
+
 export const sendMessage = async (req, res) => { 
    try {
      const { message } = req.body;
-     const {id} =req.params;
-     const senderId=req.userId;
-     res.status(200).json({ message: "Message sent successfully" });
+     const {id:receiverId} =req.params;
+     const senderId=req.user._id;
+
+     const conversation = await Conversation.findOne({ 
+        participants:{$all: [senderId, receiverId] }
+     });
+
+     if(!conversation){
+         conversation = await Conversation.create({
+             participants:[senderId, receiverId]
+         });
+     }
+
+     const newMessage = new Message({
+        senderId,
+         receiverId,
+         message
+     })
+
+     if(newMessage){
+        conversation.message.push(newMessage._id);
+     }
+
+    await Promise.message.push(conversation.save(), newMessage.save());
+
+     res.status(200).json(newMessage);
    }
     catch (error) {
     
    }
+}
+
+export const getMessage = async (req, res) => {
+    try {
+        
+        const {id:receiverId} =req.params;
+        const senderId=req.user._id;
+        const conversation = await Conversation.findOne({   
+            participants:{$all: [senderId, receiverId] }
+        }).populate("message"); 
+
+        res.status(200).json(conversation.message);
+    } 
+    catch (error)
+    {
+        console.log("Error in getMessage middleware:", error.message)
+        res.status(500).json({ error: "Internal server error" })
+    }
 }
--- BackEnd/middleware/protectRoute.js
+++ BackEnd/middleware/protectRoute.js
@@ -14,10 +14,18 @@ const protectRoute = async (req, res, next) => {
             return res.status(401).json({ error: "Unauthorized - Invalid token" });
         }
 
-        const user = await User.findById(decoded.userId);
+        const user = await User.findById(decoded.userId).select("-password");
+
+        if(!user){ 
+            return res.status(401).json({ error: "User not found" });
+        }
+
+        req.user = user;
+        next();
+
     } 
     catch (error) {
-        console.log("Error in protectRoute:", error.message)
+        console.log("Error in protectRoute middleware:", error.message)
         res.status(500).json({ error: "Internal server error" })
     }
 }
