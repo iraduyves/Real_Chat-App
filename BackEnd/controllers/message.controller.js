c9023f07af935eb89859be8357efd7d86fc7daf9
Yves IRADUKUNDA <iradukundayves11@gmail.com>
2024-02-12 20:14:44 +0200

    feat:implementing Backend

--- BackEnd/controllers/message.controller.js
+++ BackEnd/controllers/message.controller.js
@@ -38,7 +38,6 @@ export const sendMessage = async (req, res) => {
 
 export const getMessage = async (req, res) => {
     try {
-        kj
         const {id:receiverId} =req.params;
         const senderId=req.user._id;
         const conversation = await Conversation.findOne({   
