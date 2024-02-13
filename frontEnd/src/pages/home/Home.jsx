c80de9b4e8ac34b0745fd3902f61c6279c2a864e
Yves IRADUKUNDA <iradukundayves11@gmail.com>
2024-02-13 15:38:31 +0200

    feat:Add message component

--- frontEnd/src/pages/home/Home.jsx
+++ frontEnd/src/pages/home/Home.jsx
@@ -1,3 +1,4 @@
+import MessageContainer from "../../component/messages/MessageContainer"
 import Sidebar from "../../component/sidebar/Sidebar"
 
 
@@ -5,7 +6,7 @@ function Home() {
   return (
     <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
         <Sidebar/>
-         {/*<MessageContainer/> */}
+         <MessageContainer/>
     </div>
   )
 }
