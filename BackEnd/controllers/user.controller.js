35467ba3138547ba64722d05508dc1b20688434e
Yves IRADUKUNDA <iradukundayves11@gmail.com>
2024-02-13 10:29:27 +0200

    fix:fixing some bug

--- BackEnd/controllers/user.controller.js
+++ BackEnd/controllers/user.controller.js
@@ -1,9 +1,9 @@
-import {User} from "../Models/user.model";
+import User from "../Models/user.model.js";
 export const getUsersForsidebar = async (req, res) => {
     try {
         const loggedInUser = req.user._id;
 
-        // const allUsers = await User.find({ _id: { $ne: loggedInUser } });   
+        const allUsers = await User.find({ _id: { $ne: loggedInUser } });   
     } 
     catch (error) {
         console.error("Error in getUserForsidebar", error.message);
