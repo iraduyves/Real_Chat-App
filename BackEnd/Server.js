47a45e67015c53c64f22410fb33526db5d3f431d
Yves IRADUKUNDA <iradukundayves11@gmail.com>
2024-02-13 10:24:23 +0200

    fix:fixing some bug

--- BackEnd/Server.js
+++ BackEnd/Server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
+import userRoutes from './routes/user.routes.js';
 import connectToMongoDB from './db/connectToMongoDB.js';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
---
+++ BackEnd/controllers/user.controller.js
@@ -0,0 +1,12 @@
+import {User} from "../Models/user.model";
+export const getUsersForsidebar = async (req, res) => {
+    try {
+        const loggedInUser = req.user._id;
+
+        // const allUsers = await User.find({ _id: { $ne: loggedInUser } });   
+    } 
+    catch (error) {
+        console.error("Error in getUserForsidebar", error.message);
+        res.status(500).json({error:"Internal server error"})
+    }
+}
