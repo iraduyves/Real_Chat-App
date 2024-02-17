5d85b69fc17862b8de40dd23272c96f6b45a1b69
Yves IRADUKUNDA <iradukundayves11@gmail.com>
2024-02-17 14:48:14 +0200

    feat:adding context provider

---
+++ frontEnd/src/context/AuthContext.jsx
@@ -0,0 +1,20 @@
+import { createContext, useContext, useState } from "react";
+
+export const AuthContext = createContext();
+
+export const useAuthContext = () => {
+    return useContext(AuthContext);
+}
+
+
+export const AuthContextProvider = ({ children }) => {
+    const [authUser, setAuthUser] = useState(localStorage.getItem("authUser")? JSON.parse(localStorage.getItem("authUser")) : null);
+    return <AuthContext.Provider 
+      value={{
+        authUser,
+        setAuthUser
+      }}
+      >
+     {children}
+    </AuthContext.Provider>;
+}
