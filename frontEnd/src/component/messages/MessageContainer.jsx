50995cadd6401919da51d8d340ca4df3be6508b5
Yves IRADUKUNDA <iradukundayves11@gmail.com>
2024-02-18 13:10:57 +0200

    feat:intergrate user messages

--- frontEnd/src/component/messages/MessageContainer.jsx
+++ frontEnd/src/component/messages/MessageContainer.jsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import useConversation from "../../Store/UseContext";
 import MessageInput from "./MessageInput"
 import Messages from "./Messages"
 import { TiMessages } from "react-icons/ti";
@@ -5,17 +7,23 @@ import { TiMessages } from "react-icons/ti";
 
 
 function MessageContainer() {
-    const noChatSelected =false;
+    const {selectedConversation,setSelectedConversation} = useConversation();
+
+    useEffect(()=>{
+      //cleanup function(unmounts)
+      return ()=>{setSelectedConversation(null)}
+    },[setSelectedConversation])
+    
   return (
 
     <div className="md:min-w-[450px] flex flex-col">
-     {noChatSelected? 
+     {!selectedConversation? 
       (<NoChatSelected/> 
       ): (
        <>
         <div className="bg-slate-500 px-4 py-2 mb-2">
             <span className="label-text">To:
-              <span className="text-gray-900 font-bold">Jhon Doe</span>
+              <span className="text-gray-900 font-bold">{selectedConversation?.fullName}</span>
             </span>
         </div>
         <Messages/>
