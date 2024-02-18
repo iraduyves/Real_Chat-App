eee6f5166f85b830a4bd6d9a4db57ffae0a9c5bd
Yves IRADUKUNDA <iradukundayves11@gmail.com>
2024-02-18 12:02:40 +0200

    feat:intergrating login function

--- frontEnd/src/pages/login/Login.jsx
+++ frontEnd/src/pages/login/Login.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
 import { Link } from "react-router-dom"
+import UseLogin from "../../hooks/UseLogin";
 
 
 function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const {loading,login}= UseLogin()
+
+    const HandleSubmit =async(e)=>{
+        e.preventDefault();
+        await login({username,password});
+    }
   return (
    <>
      <div className="flex flex-col items-center justify-center min-w-96 max-auto">
@@ -9,23 +20,41 @@ function Login() {
             <h1 className="text-3xl font-semibold text-center text-gray-300">login
             <span className="text-blue-500">ChatApp</span>
             </h1>
-            <form>
+            <form onSubmit={HandleSubmit}>
                 <div>
                     <label htmlFor="" className="label p-2">
                         <span className="text-base label-text">Username</span>
                     </label>
-                    <input type="text" placeholder="Enter Username" className="w-full input input-bodered h-10" />
+                    <input 
+                     type="text" 
+                     placeholder="Enter Username" 
+                     className="w-full input input-bodered h-10" 
+                     value={username}
+                     onChange={(e) => setUsername(e.target.value)}
+                    />
                 </div>
                 <div>
                     <label htmlFor="" className="label ">
                         <span className="text-base label-text">Password</span>
                     </label>
-                    <input type="passsword" placeholder="Enter password" className="w-full input input-bodered h-10" />
+                    <input 
+                     type="passsword" 
+                     placeholder="Enter password" 
+                     className="w-full input input-bodered h-10" 
+                     value={password}
+                     onChange={(e) => setPassword(e.target.value)}
+                    />
                 </div>
                 <Link to='/Signup' className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block">
                     {"Don't"} have an account?
                 </Link>
-                <div className="btn btn-block btn-sm mt-2">Login</div>
+                <button 
+                 className="btn btn-block btn-sm mt-2"
+                 type="submit"
+                 disabled={loading}
+                >
+                    {loading? <span className="loading loading-spinner"></span> : "Login"}
+                </button>
             </form>
         </div>
      </div>
