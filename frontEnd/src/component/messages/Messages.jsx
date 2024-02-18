3a7a31d3a57d274d14a8e9e4ab58c7d4f57ccd33
Yves IRADUKUNDA <iradukundayves11@gmail.com>
2024-02-18 23:06:26 +0200

    feat:adding message

--- frontEnd/src/component/messages/Messages.jsx
+++ frontEnd/src/component/messages/Messages.jsx
@@ -1,15 +1,24 @@
+import useGetMessages from "../../hooks/useGetMessages";
+import MessageSkeleton from "../../skeletons/MessageSkeleton.jsx";
 import Message from "./Message"
 
 
 function Messages() {
+  const {loading,messages}=useGetMessages();
+  console.log("messages",messages);
+
   return (
     <div className="px-4 flex-1 overflow-auto">
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
+      { loading && [...Array(3)].map((_,index)=>
+         <MessageSkeleton key={index}/>)
+      }
+      {!loading &&  messages.length === 0 &&(
+        <p className="text-center">Send a message to start the conversation</p>
+      )}
+      {!loading && messages.length>0 && messages.map((message)=>{
+        <Message key={message._id} message={message}/>
+      })
+      }
     </div>
   )
 }
