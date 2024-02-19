148c383bf4485d8979b1e92af1c6ff4b0c46970a
Yves IRADUKUNDA <iradukundayves11@gmail.com>
2024-02-19 12:07:19 +0200

    feat:implementing search conversation

--- frontEnd/src/component/sidebar/SearchInput.jsx
+++ frontEnd/src/component/sidebar/SearchInput.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import { IoSearchSharp } from "react-icons/io5";
 
 function SearchInput() {
+  const [search,setSearch] =useState("") 
+
+  
+  const handleSubmit=()=>{
+
+  }
   return (
     <>
-    <form action="" className="flex items-center gap-12">
+    <form onSubmit={handleSubmit} className="flex items-center gap-12">
         <input type="text" placeholder="Search..." className="input input-bordered rounded-full" />
         <button className="btn btn-circle bg-sky-500 text-white">
             <IoSearchSharp className="w-6 h-6 outline-none"/>
