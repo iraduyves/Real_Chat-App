6c42a13f78dd105fe765371641f516532245ebd7
Yves IRADUKUNDA <iradukundayves11@gmail.com>
2024-02-19 12:01:37 +0200

    feat:sending message intergration

--- frontEnd/src/component/messages/Messages.jsx
+++ frontEnd/src/component/messages/Messages.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import useGetMessages from "../../hooks/useGetMessages";
 import MessageSkeleton from "../../skeletons/MessageSkeleton.jsx";
 import Message from "./Message"
@@ -5,20 +6,35 @@ import Message from "./Message"
 
 function Messages() {
   const {loading,messages}=useGetMessages();
-  console.log("messages",messages);
+  const lastMessageRef = useRef();
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      lastMessageRef.current.scrollIntoView({
+        behavior: "smooth",
+        block: "end",
+     })
+   }, 100);
+  },[messages])
 
   return (
     <div className="px-4 flex-1 overflow-auto">
       { loading && [...Array(3)].map((_,index)=>
          <MessageSkeleton key={index}/>)
       }
-      {!loading &&  messages.length === 0 &&(
+      {!loading &&  messages?.length === 0 &&(
         <p className="text-center">Send a message to start the conversation</p>
       )}
-      {!loading && messages.length>0 && messages.map((message)=>{
-        <Message key={message._id} message={message}/>
-      })
+      {!loading && 
+        messages?.length > 0 &&
+        messages.map((message) => (
+          <div key={message._id} ref={lastMessageRef}>
+            <Message  message={message} />
+          </div>
+        ))
       }
+      {/* {!loading && messages?.length>0 && <Message message={messages}/>
+      } */}
     </div>
   )
 }
