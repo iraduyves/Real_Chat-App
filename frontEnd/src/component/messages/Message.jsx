7e5fc5c72fb0d0041c221bd48a2af767e53b224b
Yves IRADUKUNDA <iradukundayves11@gmail.com>
2024-02-19 12:00:20 +0200

    feat:adding dependencies

--- frontEnd/src/component/messages/Message.jsx
+++ frontEnd/src/component/messages/Message.jsx
@@ -1,24 +1,26 @@
 import useConversation from "../../Store/UseContext"
-import { useAuthContext } from "../../context/AuthContext"
+import  {useAuthContext}  from "../../context/AuthContext"
+import { extractTime } from "../../utils/exactTime"
 
 
 function Message({message}) {
   const {authUser}=useAuthContext()
   const {selectedConversation}=useConversation()
-  const fromMe = messaage.senderId === authUser._id
+  const fromMe = message?.senderId == authUser?._id
   const chatClassName = fromMe ? 'chat chat-end' : 'chat chat-start'
-  const profilePic = fromMe ? authUser?.profilePic : selectedConversation?.profilePicture
+  const profilePic = !fromMe ? authUser?.profilePic : selectedConversation?.profilePic
   const bubbleBgColor = fromMe ? 'bg-blue-500' : ''
+  const formattedTime= extractTime(message?.createdAt)
 
   return (
     <div className={`chat ${chatClassName}`}>
-        <div className={`chat-image ${profilePic}`}>
+        <div className={`chat-image avatar`}>
             <div className="w-10 rounded-full">
-                <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" alt="sender image" />
+                <img src={`${profilePic}`} alt="sender image" />
             </div>
         </div>
-        <div className={`chat-bubble text-white ${bubbleBgColor}`}>{message?.message}</div>
-        <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">09:00</div>
+        <div className={`chat-bubble text-white ${bubbleBgColor} pb-2`}>{message?.message}</div>
+        <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">{formattedTime}</div>
     </div>
   )
 }
