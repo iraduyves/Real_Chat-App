b5bb8f70daaca600e0258ed4d625847369902ce9
Yves IRADUKUNDA <iradukundayves11@gmail.com>
2024-02-22 23:33:53 +0200

    feat: implementing socket

--- frontEnd/src/context/SocketContext.jsx
+++ frontEnd/src/context/SocketContext.jsx
@@ -8,6 +8,7 @@ export const useSocketContext = () => {
 	return useContext(SocketContext);
 };
 
+
 export const SocketContextProvider = ({ children }) => {
 	const [socket, setSocket] = useState(null);
 	const [onlineUsers, setOnlineUsers] = useState([]);
