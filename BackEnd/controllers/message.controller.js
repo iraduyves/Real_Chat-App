e6c3a2d37bef38ac65c1e0a207e7e524b0bbb203
Yves IRADUKUNDA <iradukundayves11@gmail.com>
2024-02-23 22:42:54 +0200

    feat: implementing some changes on message controllers

--- BackEnd/controllers/message.controller.js
+++ BackEnd/controllers/message.controller.js
@@ -42,7 +42,6 @@ export const sendMessage = async (req, res) => {
 
 export const getMessage = async (req, res) => {
 
-    
     try {
         const {id:receiverId} =req.params;
         const senderId=req.user._id;
